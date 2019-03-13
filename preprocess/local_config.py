9e2f9cac1a601f5325f374a4c6ea4e1d3c6803bf
albincorreya <albin.a.correya@gmail.com>
2019-03-13 15:15:13 +0100

    update local_config paths

--- preprocess/local_config.py
+++ preprocess/local_config.py
@@ -12,8 +12,8 @@ FEATURE_DIR = PROJECT_PATH + "features/"
 MODULE_PATH = PROJECT_PATH + "coversongdataset/"
 SUBSETS_DIR = MODULE_PATH + "subsets/"
 COLLECTION_DIR = SUBSETS_DIR + "collection_txts_furkan/" 
-BENCHMARK_SUBSET_FILE = SUBSETS_DIR + "benchmark_subset_paths_v2.txt"
-WHATISACOVER_SUBSET_FILE = SUBSETS_DIR + "whatisacover_subset_paths_v2.txt"
+BENCHMARK_SUBSET_FILE = SUBSETS_DIR + "benchmark_subset_paths.txt"
+WHATISACOVER_SUBSET_FILE = SUBSETS_DIR + "whatisacover_subset_paths.txt"
 LOG_PATH = MODULE_PATH + "preprocess/logs/"
 
 
