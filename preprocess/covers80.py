18ed51fdf13eae812d00066deb68acda90b5e026
Chris Tralie <chris.tralie@gmail.com>
2019-03-24 13:55:24 -0400

    Code for computing covers80 features, for initial testing of benchmarks

---
+++ preprocess/covers80.py
@@ -0,0 +1,24 @@
+"""
+Compute features for the covers80 dataset.  This assumes
+that the mp3 files have been downloaded from 
+https://labrosa.ee.columbia.edu/projects/coversongs/covers80/covers80.tgz
+and the subdirectory "covers32" has been placed at the root of this directory
+"""
+from extractors import compute_features_from_list_file
+
+def compute_covers80_features():
+    fin = open("covers32k/list1.list")
+    files = fin.readlines()
+    fin.close()
+    fin = open("covers32k/list2.list")
+    files += fin.readlines()
+    fin.close()
+    files = ["covers32k/%s.mp3"%s.strip() for s in files]
+    fout = open("covers80.txt", "w")
+    for f in files:
+        fout.write("%s\n"%f)
+    fout.close()
+    compute_features_from_list_file("covers80.txt", "covers80/")
+
+if __name__ == '__main__':
+    compute_covers80_features()
