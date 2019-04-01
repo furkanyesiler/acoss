fc90974fbe070ecf36476dbc205d828f7c34d79a
Chris Tralie <chris.tralie@gmail.com>
2019-04-01 18:47:25 -0400

    Pulling out some of the code for dealing with OTI and CRPs from EarlyFusion and putting it in its own utility file, which I share with Serra09.  Also downsampling Serra09 HPCPs to be closer to the paper

--- benchmarking/EarlyFusion.py
+++ benchmarking/EarlyFusion.py
@@ -1,156 +1,14 @@
 import numpy as np
-import scipy
 import matplotlib.pyplot as plt
 from CoverAlgorithm import *
 from SimilarityFusion import *
 import argparse
 from pySeqAlign import swconstrained as alignment_fn
-
+from CRPUtils import *
 
 """====================================================
                 UTILITY FUNCTIONS
 ===================================================="""
-
-def get_ssm(X):
-    """
-    Fast code for computing the self-similarity matrix of a point cloud
-    Parameters
-    ----------
-    X: ndarray(N, d)
-        A point cloud with N points in d dimensions
-    Returns
-    -------
-    d: ndarray(N, N)
-        All pairs similarity matrix
-    """
-    XSqr = np.sum(X**2, 1)
-    DSqr = XSqr[:, None] + XSqr[None, :] - 2*X.dot(X.T)
-    DSqr[DSqr < 0] = 0
-    np.fill_diagonal(DSqr, 0)
-    return np.sqrt(DSqr)
-
-def get_csm(X, Y):
-    """
-    Return the Euclidean cross-similarity matrix between the M points
-    in the Mxd matrix X and the N points in the Nxd matrix Y.
-    Parameters
-    ----------
-    X: ndarray(M, d)
-        A point cloud with M points in d dimensions
-    Y: ndarray(N, d)
-        A point cloud with N points in d dimensions
-    Returns
-    -------
-    D: ndarray(M, N)
-        An MxN Euclidean cross-similarity matrix
-    """
-    C = np.sum(X**2, 1)[:, None] + np.sum(Y**2, 1)[None, :] - 2*X.dot(Y.T)
-    C[C < 0] = 0
-    return np.sqrt(C)
-
-def get_csm_cosine(X, Y):
-    """
-    Return the cosine distance between all vectors in X
-    and all vectors in Y
-    X: ndarray(M, d)
-        A point cloud with M points in d dimensions
-    Y: ndarray(N, d)
-        A point cloud with N points in d dimensions
-    Returns
-    -------
-    D: ndarray(M, N)
-        A cosine cross-similarity matrix
-    """
-    XNorm = np.sqrt(np.sum(X**2, 1))
-    XNorm[XNorm == 0] = 1
-    YNorm = np.sqrt(np.sum(Y**2, 1))
-    YNorm[YNorm == 0] = 1
-    D = (X/XNorm[:, None]).dot((Y/YNorm[:, None]).T)
-    D = 1 - D #Make sure distance 0 is the same and distance 2 is the most different
-    return D
-
-def get_oti(C1, C2, do_plot = False):
-    """
-    Get the optimial transposition of the first chroma vector
-    with respect to the second one
-    Parameters
-    ----------
-    C1: ndarray(n_chroma_bins)
-        Chroma vector 1
-    C2: ndarray(n_chroma_bins)
-        Chroma vector 2
-    do_plot: boolean
-        Plot the agreements over all shifts
-    Returns
-    -------
-    oit: int
-        An index by which to rotate the first chroma vector
-        to match with the second
-    """
-    NChroma = len(C1)
-    shiftScores = np.zeros(NChroma)
-    for i in range(NChroma):
-        shiftScores[i] = np.sum(np.roll(C1, i)*C2)
-    if do_plot:
-        plt.plot(shiftScores)
-        plt.title("OTI")
-        plt.show()
-    return np.argmax(shiftScores)
-
-def get_csm_blocked_cosine_oti(X, Y, C1, C2):
-    """
-    Get the cosine distance between each row of X
-    and each row of Y after doing a global optimal
-    transposition change from X to Y
-    Parameters
-    ----------
-    X: ndarray(M, n_chroma_bins*chromas_per_block)
-        Chroma blocks from first song
-    Y: ndarray(N, n_chroma_bins*chromas_per_block)
-        Chroma blocks from the second song
-    C1: ndarray(n_chroma_bins)
-        Global chroma vector for song 1
-    C2: ndarray(n_chroma_bins)
-        Global chroma vector for song 2
-    Returns
-    -------
-    D: ndarray(M, N)
-        An MxN cross-similarity matrix
-    """
-    NChromaBins = len(C1)
-    ChromasPerBlock = int(X.shape[1]/NChromaBins)
-    oti = get_oti(C1, C2)
-    X1 = np.reshape(X, (X.shape[0], ChromasPerBlock, NChromaBins))
-    X1 = np.roll(X1, oti, axis=2)
-    X1 = np.reshape(X1, [X.shape[0], ChromasPerBlock*NChromaBins])
-    return get_csm_cosine(X1, Y)
-
-
-def csm_to_binary(D, kappa):
-    """
-    Turn a cross-similarity matrix into a binary cross-simlarity matrix, using partitions instead of
-    nearest neighbors for speed
-    :param D: M x N cross-similarity matrix
-    :param kappa:
-        If kappa = 0, take all neighbors
-        If kappa < 1 it is the fraction of mutual neighbors to consider
-        Otherwise kappa is the number of mutual neighbors to consider
-    :returns B: MxN binary cross-similarity matrix
-    """
-    N = D.shape[0]
-    M = D.shape[1]
-    if kappa == 0:
-        return np.ones_like(D)
-    elif kappa < 1:
-        NNeighbs = int(np.round(kappa*M))
-    else:
-        NNeighbs = kappa
-    J = np.argpartition(D, NNeighbs, 1)[:, 0:NNeighbs]
-    I = np.tile(np.arange(N)[:, None], (1, NNeighbs))
-    V = np.ones(I.size)
-    [I, J] = [I.flatten(), J.flatten()]
-    ret = sparse.coo_matrix((V, (I, J)), shape=(N, M), dtype=np.uint8)
-    return ret.toarray()
 
 def resize_block(X, i1, i2, frames_per_block, median_aggregate = False):
     """
@@ -345,8 +203,9 @@ class EarlyFusion(CoverAlgorithm):
             CSMs['ssms'] = get_csm(feats1['ssms'], feats2['ssms'])
             D *= 0
             scores['ssms'] = alignment_fn(csm_to_binary(CSMs['ssms'], self.kappa).flatten(), D, M, N)
-            CSMs['chromas'] = get_csm_blocked_cosine_oti(feats1['chromas'], feats2['chromas'], \
-                                                        feats1['chroma_med'], feats2['chroma_med'])
+            CSMs['chromas'] = get_csm_blocked_oti(feats1['chromas'], feats2['chromas'], \
+                                                        feats1['chroma_med'], feats2['chroma_med'],\
+                                                        get_csm_cosine)
             D *= 0
             scores['chromas'] = alignment_fn(csm_to_binary(CSMs['chromas'], self.kappa).flatten(), D, M, N)
             
--- benchmarking/Serra09.py
+++ benchmarking/Serra09.py
@@ -1,108 +1,17 @@
 # -*- coding: utf-8 -*-
-"""
-"""
-from sklearn.metrics.pairwise import euclidean_distances
 from pySeqAlign import qmax
 from CoverAlgorithm import *
+from CRPUtils import *
 import numpy as np
 import argparse
+import librosa
 
 
-def global_hpcp(chroma):
-    """Computes global hpcp of a input chroma vector"""
+def global_chroma(chroma):
+    """Computes global chroma of a input chroma vector"""
     if chroma.shape[1] not in [12, 24, 36]:
         raise IOError("Wrong axis for the input chroma array. Expected shape '(frame_size, bin_size)'")
     return np.divide(chroma.sum(axis=0), np.max(chroma.sum(axis=0)))
-
-
-def optimal_transposition_index(chromaA, chromaB, n_shifts=12):
-    """
-    Computes optimal transposition index (OTI) for the chromaB to be transposed in the same key as of chromaA
-    Input :
-            chromaA : chroma feature array of the query song for reference key
-            chromaB : chroma feature array of the reference song for which OTI has to be applied
-        Params:
-                n_shifts: (default=12) Number of oti tranpositions to be checked for circular shift
-    Output : Integer value specifying optimal transposition index for transposing chromaB to chromaA to be in same key
-    """
-    global_hpcpA = global_hpcp(chromaA)
-    global_hpcpB = global_hpcp(chromaB)
-    idx = list()
-    for index in range(n_shifts):
-        idx.append(np.dot(global_hpcpA, np.roll(global_hpcpB, index)))
-    return int(np.argmax(idx))
-
-
-def transpose_by_oti(chromaB, oti=0):
-    """
-    Transpose the chromaB vector to a common key by a value of optimal transposition index
-    Input :
-            chromaB : input chroma array        
-    Output : chromaB vector transposed to a factor of specified OTI
-    """
-    return np.roll(chromaB, oti)
-
-
-def to_embedding(input_array, tau=1, m=9):
-    """
-    Construct a time series with delay embedding 'tau' and embedding dimension 'm' from the input audio feature vector
-    Input :
-            input_array : input feature array for constructing the stacked features
-    Params :
-            tau (default : 1): delay embedding
-            m (default : 9): embedding dimension
-
-    Output : Time series representation of the input audio feature vector
-    """
-
-    timeseries = list()
-    for startTime in range(0, input_array.shape[0] - m*tau, tau):
-        stack = list()
-        for idx in range(startTime, startTime + m*tau, tau):
-            stack.append(input_array[idx])
-        timeseries.append(np.ndarray.flatten(np.array(stack)))
-    return np.array(timeseries)
-
-
-
-def cross_recurrent_plot(input_x, input_y, tau=1, m=9, kappa=0.095, oti=True):
-    """
-    Constructs the Cross Recurrent Plot of two audio feature vector as mentioned in [1]
-    Inputs :
-            input_x : input feature array of query song
-            input_y : input feature array of reference song
-    Params :
-            kappa (default=0.095)       : fraction of mutual nearest neighbours to consider [0, 1]
-            tau (default=1)             : delay embedding [1, inf]
-            m (default=9)               : embedding dimension for the time series embedding [0, inf]
-            swapaxis (default=False)    : swapaxis of the feature array if it not in the shape (x,12) where x is the \
-                                          time axis
-            oti (default=True)    : boolean to check to choose if OTI should be applied to the reference song
-
-    Output : Binary similarity matrix where 1 constitutes a similarity between
-            two feature vectors at ith and jth position respectively and 0 denotes non-similarity
-    """
-
-    if oti:
-        oti_idx = optimal_transposition_index(input_x, input_y)
-        input_y = transpose_by_oti(input_y, oti_idx) #transpose input_y to the key of input_x by a oti value
-
-    pdistances = euclidean_distances(input_x, input_y)
-    transposed_pdistances = pdistances.T
-
-    eph_x = np.percentile(pdistances, kappa*100, axis=1)
-    eph_y = np.percentile(transposed_pdistances, kappa*100, axis=1)
-    x = eph_x[:,None] - pdistances
-    y = eph_y[:,None] - transposed_pdistances
-
-    #apply step function to the array (Binarize the array)
-    x = np.piecewise(x, [x<0, x>=0], [0,1])
-    y = np.piecewise(y, [y<0, y>=0], [0,1])
-
-    crp = x*y.T
-    return crp
-
-
 
 class Serra09(CoverAlgorithm):
     """
@@ -111,12 +20,16 @@ class Serra09(CoverAlgorithm):
     Same as CoverAlgorithms, plus
     chroma_type: string
         Type of chroma to use (key into features)
+    downsample_fac: int
+        The factor by which to downsample the HPCPs with
+        median aggregation
     shapes: {int: int}
         Shapes of each song, used for normalization
+    all_feats: {int: dictionary}
+        Cached features
     """
     def __init__(self, datapath="../features_covers80", chroma_type='hpcp', shortname='benchmark', 
-                oti=True, kappa=0.095, tau=1, m=9):
-
+                oti=True, kappa=0.095, tau=1, m=9, downsample_fac=40):
         self.oti = oti
         self.tau = tau
         self.m = m
@@ -124,26 +37,37 @@ class Serra09(CoverAlgorithm):
         self.kappa = kappa
         self.tau = tau
         self.m = m
+        self.downsample_fac = downsample_fac
         self.shapes = {}
-        CoverAlgorithm.__init__(self, "QMAX", datapath=datapath, shortname=shortname)
+        self.all_feats = {} # For caching features (global chroma and stacked chroma)
+
+        CoverAlgorithm.__init__(self, "Serra09", datapath=datapath, shortname=shortname)
 
     def load_features(self, i):
-        feats = CoverAlgorithm.load_features(self, i)
-        chroma = feats[self.chroma_type].T
-        return to_embedding(chroma, tau=self.tau, m=self.m)
+        if not i in self.all_feats:
+            feats = CoverAlgorithm.load_features(self, i)
+            # First compute global chroma (used for OTI later)
+            chroma = feats[self.chroma_type]
+            gchroma = global_chroma(chroma)
+            # Now downsample the chromas using median aggregation
+            chroma = librosa.util.sync(chroma.T, np.arange(0, chroma.shape[0], self.downsample_fac), aggregate=np.median)
+            # Finally, do a stacked delay embedding
+            stacked = librosa.feature.stack_memory(chroma, self.tau, self.m).T
+            feats = {'gchroma':gchroma, 'stacked':stacked}
+            self.all_feats[i] = feats
+        return self.all_feats[i]
 
     def similarity(self, idxs):
-        print(idxs)
         for i,j in zip(idxs[:, 0], idxs[:, 1]):
-            query_feature = self.load_features(i)
-            reference_feature = self.load_features(j)
-            csm = cross_recurrent_plot(query_feature, reference_feature, kappa=self.kappa, oti=self.oti)
-            csm = csm.astype(dtype=np.uint8)
+            print(i, j)
+            Si = self.load_features(i)
+            Sj = self.load_features(j)
+            csm = get_csm_blocked_oti(Si['stacked'], Sj['stacked'], Si['gchroma'], Sj['gchroma'], get_csm_euclidean)
+            csm = csm_to_binary(csm, self.kappa)
             M, N = csm.shape[0], csm.shape[1]
             D = np.zeros(M*N, dtype=np.float32)
             score = qmax(csm.flatten(), D, M, N)
             self.shapes[j] = N
-            #score = np.sqrt(csm.shape[1]) / score
             for key in self.Ds.keys():
                 self.Ds[key][i][j] = score
     
@@ -158,8 +82,6 @@ class Serra09(CoverAlgorithm):
                     # the evaluation statistics assume something with a higher
                     # score is more similar
                     self.Ds[key][i, j] = self.Ds[key][i, j] / np.sqrt(self.shapes[j])
-
-
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Benchmarking with Joan Serra's Cover id algorithm",
@@ -176,12 +98,12 @@ if __name__ == '__main__':
 
     cmd_args = parser.parse_args()
 
-    qmax = Serra09(cmd_args.datapath, cmd_args.chroma_type, cmd_args.shortname)
-    qmax.all_pairwise(cmd_args.parallel, cmd_args.n_cores, symmetric=True)
-    qmax.normalize_by_length()
-    for similarity_type in qmax.Ds.keys():
+    serra09 = Serra09(cmd_args.datapath, cmd_args.chroma_type, cmd_args.shortname)
+    serra09.all_pairwise(cmd_args.parallel, cmd_args.n_cores, symmetric=True)
+    serra09.normalize_by_length()
+    for similarity_type in serra09.Ds.keys():
         print(similarity_type)
-        qmax.getEvalStatistics(similarity_type)
-    qmax.cleanup_memmap()
+        serra09.getEvalStatistics(similarity_type)
+    serra09.cleanup_memmap()
     print("... Done ....")
 
