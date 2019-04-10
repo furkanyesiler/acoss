1dbf896ecdafaaeb807279cdaa87bf43aa5e256d
Philip Tovstogan <phil.tgun@gmail.com>
2019-04-10 17:03:41 +0200

    Finished stats script

--- SHS_scraper/stats.py
+++ SHS_scraper/stats.py
@@ -1,6 +1,11 @@
 import json
 import argparse
-from SHS_scraper.musicbrainz_querying import load_json
+
+
+def load_json(filename):
+    with open(filename) as fp:
+        data = json.load(fp)
+    return data
 
 
 def get_stats(input_filename):
@@ -11,7 +16,8 @@ def get_stats(input_filename):
     work_counter = 0
     perf_counter = 0
     have_length = 0
-    have_mbid = 0
+    have_mbids = 0
+    have_many_mbids = 0
     have_tags = 0
 
     for work_id, work in data.items():
@@ -19,6 +25,27 @@ def get_stats(input_filename):
         for perf_id, performance in work.items():
             perf_counter += 1
 
+            if 'mb_performances' in performance:
+                have_mbids += 1
+
+                if len(performance['mb_performances']) > 1:
+                    have_many_mbids += 1
+
+                has_length = False
+                has_tags = False
+                for mbid, mb_performance in performance['mb_performances'].items():
+                    has_length |= 'length' in mb_performance
+                    has_tags |= 'tags' in mb_performance
+
+                have_length += int(has_length)
+                have_tags += int(has_tags)
+
+    print('Total works: {}'.format(work_counter))
+    print('Total performances: {}'.format(perf_counter))
+    print('- have mbids: {}'.format(have_mbids))
+    print('- have more than one mbid: {}'.format(have_many_mbids))
+    print('- have length: {}'.format(have_length))
+    print('- have tags: {}'.format(have_tags))
 
 
 if __name__ == '__main__':
