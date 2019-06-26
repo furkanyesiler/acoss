05c5fa92099ca1f7c322ca819e654910691ec877
furkanyesiler <furkan.yesiler@gmail.com>
2019-06-26 15:34:29 +0200

    change path

--- preprocess/local_config.py
+++ preprocess/local_config.py
@@ -6,12 +6,12 @@ import preprocess.utils as util
 import os
 
 
-PROJECT_PATH = "/datasets/MTG/projects/tacos/"
+PROJECT_PATH = "/datasets/MTG/projects/da-tacos/"
 SONG_DIR = PROJECT_PATH + "downloads/songs/"
 FEATURE_DIR = PROJECT_PATH + "features/"
 MODULE_PATH = PROJECT_PATH + "coversongdataset/"
 SUBSETS_DIR = MODULE_PATH + "subsets/"
-COLLECTION_DIR = SUBSETS_DIR + "collection_txts_furkan/"
+COLLECTION_DIR = SUBSETS_DIR + "collection_txts/"
 BENCHMARK_SUBSET_FILE = SUBSETS_DIR + "benchmark_subset_paths.txt"
 WHATISACOVER_SUBSET_FILE = SUBSETS_DIR + "whatisacover_subset_paths.txt"
 LOG_PATH = MODULE_PATH + "preprocess/logs/"
