9c27f80743c80d7dcc76420017ef0b7ccadbad01
Albin <albin.a.correya@gmail.com>
2019-10-14 18:31:38 +0200

    only compile wheels with python3.6

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -2,10 +2,13 @@
 set -e -x
 
 # Compile wheels
-for PYBIN in /opt/python/*/bin; do
-    "${PYBIN}/pip" install -r /io/travis/dev-requirements.txt
-    "${PYBIN}/pip" wheel /io/ -w wheelhouse/
-done
+#for PYBIN in /opt/python/*/bin; do
+#    "${PYBIN}/pip" install -r /io/travis/dev-requirements.txt
+#    "${PYBIN}/pip" wheel /io/ -w wheelhouse/
+#done
+PYBIN=python3.6
+"${PYBIN}/pip" install -r /io/travis/dev-requirements.txt
+"${PYBIN}/pip" wheel /io/ -w wheelhouse/
 
 # Bundle external shared libraries into the wheels
 for whl in wheelhouse/*.whl; do
