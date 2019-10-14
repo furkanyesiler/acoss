d1f658082d1fdb745a9df020467311121aad2ac9
Albin <albin.a.correya@gmail.com>
2019-10-14 14:28:24 +0200

    update travis build settings

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e -x
 
-# Install a system package required by our library
-yum install -y atlas-devel
+# Build tools for using original quay.io/pypa/manylinux1_* docker images
+/io/travis/build_tools.sh
 
 # Compile wheels
 for PYBIN in /opt/python/*/bin; do
-    "${PYBIN}/pip" install -r /io/dev-requirements.txt
+    "${PYBIN}/pip" install -r /io/travis/dev-requirements.txt
     "${PYBIN}/pip" wheel /io/ -w wheelhouse/
 done
 
@@ -17,6 +17,6 @@ done
 
 # Install packages and test
 for PYBIN in /opt/python/*/bin/; do
-    "${PYBIN}/pip" install python-manylinux-demo --no-index -f /io/wheelhouse
-    (cd "$HOME"; "${PYBIN}/nosetests" pymanylinuxdemo)
+    "${PYBIN}/pip" install acoss --no-index -f /io/wheelhouse
+    (cd "$HOME"; "${PYBIN}/python -c 'from acoss import features; from pySeqAlign import qmax'")
 done
